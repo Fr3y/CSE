285ba6ad29729deee56c2395a4f740ba8c428d0f
Fr3y <Free3y@gmail.com>
2021-10-23 23:46:21 +0530

    Introduction in c

--- C/Readme.md
+++ C/Readme.md
@@ -1,1 +1,5 @@
-CSE (2021)
+### CSE (2021)
+So here you are going to see all my *C programming* 
+_i am in Second Year (3rd sem) student and i thought why not make a open source repo so others students can also contribute on it_
+
+For making it look cool and useful i am going to cover everthing in C but in case i didn't able to do that or missed something you can add that here brcause its a open sorce and always will be 
---
+++ C/introduction.c
@@ -0,0 +1,18 @@
+/* 
+ * Introduction to C 
+ * C is a procedural language which means dividing giant programs into smalls functions and parts so ve can work on it easily 
+ * the c programming is a iddle level language but it can be used as a low level too 
+ * that's why people do confused c with other low level programming languages
+ * so if you wanna understand difference between High Level language and low level it will be simply put in like this way that 
+ * that in high level language = less efforts (high level of abstraction) 
+ * low level = more efforts (low level of abstraction)
+ *
+ * Features of C 
+ * Direct access to memory through pointers
+ * bit manipulation using bitwise operators 
+ * writing assembly code within C code (my fav)
+ *
+ *
+ * C also provide wide variety of built in fuctions standard lib and header files 
+ * now we will print out hello world or anything in next part 
+ */
