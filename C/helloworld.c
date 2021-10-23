130da2816452b23c2670579702e6af6fd6b8508f
Fr3y <Free3y@gmail.com>
2021-10-24 00:05:29 +0530

    Print Hello World

---
+++ C/helloworld.c
@@ -0,0 +1,24 @@
+/*
+ * Date :- 24-10-2021
+ * Printing Hello World but you can print anything you want with printf function xD
+ * open source 
+ * part of https://github.com/Fr3y/CSE
+ */
+
+#include <stdio.h> // is called as pre-processor directive and <stdio.h> is standard input output file with .h extension 
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
+
+/*
+ * SYNTEX OF FUNCTION
+ *
+ * return_typename_of_function(paramaters_type name _of_paramaters
+ * parameter_type name_of_parameter,..)
+ * {
+ * 	statement;
+ * }
+ */
+
