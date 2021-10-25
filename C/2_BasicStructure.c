98dab71874b0f536022fb7d6868b86a998fdff90
Fr3y <Free3y@gmail.com>
2021-10-25 16:22:59 +0530

    C Programming Structure

---
+++ C/2_BasicStructure.c
@@ -0,0 +1,23 @@
+/*
+ * so as we have printed out hello world in 1_helloworld.c
+ * now we are going to understand the whole code/Structure 
+*/
+
+#include <stdio.h>
+int main()
+{
+  printf("Fr3y");
+  return 0;
+}
+
+/* now from starting we have 
+ * #include <stdio.h> it is a pre processing command
+ * it simply means that we want a file name of stdio.h 
+ * int main = is a function we use function to break down the codes into piece
+ * tip : int main is the starting point of the program
+ * we can also use main() main means the starting point but now what is int here ? 
+ * well int is an integer value now if we use int it means it will retuen int (a number)
+ * now printf() is another function from stdio.h
+ * now the last one return 0 it will retuen 0 to the OS 
+*/
+
---
+++ C/3_AddNumbers.c
@@ -0,0 +1,22 @@
+/*
+ * 25-10-2021
+ * adding numbers 
+ * fr3y
+*/
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter first number :- ");
+    scanf("%d", &a);
+
+    printf("Enter another number :- ");
+    scanf("%d", &b);
+
+    printf("the sum of numbers is :- %d", a+b);
+    return 0;
+}
+
+
+
