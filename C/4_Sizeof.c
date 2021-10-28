6fe85bcd943727ffee8149d8a72e4b95729401e3
Fr3y <Free3y@gmail.com>
2021-10-28 21:37:01 +0530

    Size of Data Types in C and one HTML file

---
+++ C/4_Sizeof.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+        printf("the size of int :  %1d \n", sizeof(int));
+        printf("the size of float : %1d \n", sizeof(float));
+        printf("the size of char : %1d \n", sizeof(char));
+        printf("the size of double : %1d \n", sizeof(double));
+        return 0;
+}
